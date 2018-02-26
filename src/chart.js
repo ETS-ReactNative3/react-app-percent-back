21beccbe358e767f8eaab48f50c05aca3a46bc26
Blaze Fugina <blaze.fugina@gmail.com>
2018-02-26 14:28:51 -0600

    update to breadcrumb error.

--- src/chart.js
+++ src/chart.js
@@ -37,7 +37,7 @@ class Chart extends React.Component {
                     <Breadcrumb>
                         <Breadcrumb.Item href="/">Enter Races</Breadcrumb.Item>
                         <Breadcrumb.Item href="/table">See Standings</Breadcrumb.Item>
-                        <Breadcrumb.Item active='/chart'>See Chart</Breadcrumb.Item>
+                        <Breadcrumb.Item active>See Chart</Breadcrumb.Item>
                     </Breadcrumb>
                 </div>
                 <h1 className="title">Chart Breakdown</h1>
