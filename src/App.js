35fc871e941bb57657865b424cd68249947ded0f
Blaze Fugina <blaze.fugina@gmail.com>
2018-02-28 15:34:19 -0600

    update.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import Input from './input';
 const FourOhFour = () => <h1 class='FourOhFour'>404</h1>
 
 const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Input} />
-        <Route exact path="/table" component={Table} />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-
+ <BrowserRouter>
+   <div className="app">
+     <Switch>
+       <Route exact path="/" component={Input} />
+       <Route exact path="/table" component={Table} />
+       <Route component={FourOhFour} />
+     </Switch>
+   </div>
+ </BrowserRouter>
 )
 
 export default App;
+
