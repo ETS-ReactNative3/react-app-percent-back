522d2cbdbf10a7e3f2d51add3bc24cf0750d8e9d
Blaze Fugina <blaze.fugina@gmail.com>
2018-03-13 10:01:55 -0500

    Fixed state problems.

--- src/chart.js
+++ src/chart.js
@@ -8,35 +8,36 @@ let datesIn = [];
 const data = {
     labels: datesIn,
     datasets: [
-      {
-        label: 'Races',
-        fill: true,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: percentBackIn
-      }
+        {
+            label: 'Races',
+            fill: true,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: percentBackIn
+        }
     ]
-  };
+};
 
 class Chart extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            races: []
+            races: [],
+            isLoaded: false
         }
         this.getRaces = this.getRaces.bind(this);
     }
@@ -57,14 +58,16 @@ class Chart extends React.Component {
                 this.organizeChartData()
             })
             .catch(error => console.log('Error fetching races', error))
-            
     }
 
     organizeChartData() {
-        for(var i = 0; i < this.state.races.length; i += 1){
+        for (var i = 0; i < this.state.races.length; i += 1) {
             percentBackIn.push(this.state.races[i].percentBack)
             datesIn.push(this.state.races[i].raceDate);
         }
+        this.setState({
+            isLoaded: true
+        })
     }
 
     render() {
@@ -83,7 +86,7 @@ class Chart extends React.Component {
                 </div>
                 <h1 className="title">Chart Breakdown</h1>
                 <div className="container">
-                    <Line data={data} redraw/>
+                    {this.state.isLoaded ? <Line data={data} redraw={true} /> : <div>Still Loading... </div>}
                 </div>
             </div>
         )
