d23f292a9f821e48770f65a1dd02956d16c3a1fd
Blaze Fugina <blaze.fugina@gmail.com>
2018-04-04 18:28:08 -0500

    Working on nav.

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+<Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+        <Navbar.Brand>
+            <a href="#brand">Races Input</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+        <Nav>
+            <NavItem eventKey={1} href="/">
+            Enter Races
+            </NavItem>
+            <Nav
+</Navbar>
