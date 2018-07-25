f927514b2016e9e8e8c96cf95f8e5568c65bfd8f
Blaze Fugina <blaze.fugina@gmail.com>
2018-07-24 22:33:03 -0500

    Let's try this routing.

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ const App = () => (
   <BrowserRouter>
     <div className="app">
       <Switch>
-        <Route exact path='/' component={Login} />
-        <Route exact path='/register' component={Register} />
-        <Route exact path="/input" component={Input} />
-        <Route exact path="/table" component={Table} />
-        <Route exact path="/chart" component={Chart} />
+        <Route exact path='./' component={Login} />
+        <Route exact path='./register' component={Register} />
+        <Route exact path="./input" component={Input} />
+        <Route exact path="./table" component={Table} />
+        <Route exact path="./chart" component={Chart} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
