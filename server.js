4c596e611afe20c0d8318254533f780095283518
Blaze Fugina <blaze.fugina@gmail.com>
2018-08-19 21:46:41 -0500

    Trying to fix routing issue.

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const auth = require('./routes/usersRouter');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
 app.use(bodyParser.json());
 //app.use(express.static('server/public'));
 
