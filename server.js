76f2a5c28b143299ff3d096523d507846f57af28
Blaze Fugina <blaze.fugina@gmail.com>
2018-08-20 22:33:39 -0500

    Let's try this implementation.

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ const races = require('./routes/racesRouter.js');
 const router = express.Router();
 const auth = require('./routes/usersRouter');
 
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/', function(req, res) {
+//     res.sendFile('./build/index.html');
+// });
+
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
-    res.sendFile('./build/index.html');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use(bodyParser.json());
