83bf95a868304cbfee4db71a23908ef1fb64e0d8
Blaze Fugina <blaze.fugina@gmail.com>
2018-08-27 23:04:12 -0500

    Change in package.json.

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const auth = require('./routes/usersRouter');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.use(bodyParser.json());
 //app.use(express.static('server/public'));
